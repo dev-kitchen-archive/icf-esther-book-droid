cfa204cd225360e3bff81ebe077e551ca2efefc5
cnworks <christianoetzli@gmail.com>
2016-02-18 15:00:10 +0100

    initial commit after rebuild with master/detail

---
+++ app/src/main/java/kitchen/dev/books_proto/BarcodeScannerActivity.java
@@ -0,0 +1,78 @@
+package kitchen.dev.books_proto;
+
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.CompoundBarcodeView;
+
+import java.util.List;
+
+public class BarcodeScannerActivity extends AppCompatActivity {
+
+    private CompoundBarcodeView barcodeView;
+
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if (result.getText() != null) {
+                barcodeView.setStatusText(result.getText());
+            }
+            //Added preview of scanned barcode
+            ImageView imageView = (ImageView) findViewById(R.id.barcodePreview);
+            imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW));
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_barcode_scanner);
+
+        barcodeView = (CompoundBarcodeView) findViewById(R.id.barcode_scanner);
+        barcodeView.decodeContinuous(callback);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        barcodeView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        barcodeView.pause();
+    }
+
+    public void pause(View view) {
+        barcodeView.pause();
+    }
+
+    public void resume(View view) {
+        barcodeView.resume();
+    }
+
+    public void triggerScan(View view) {
+        barcodeView.decodeSingle(callback);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+}
---
+++ app/src/main/java/kitchen/dev/books_proto/IntroPageFragment.java
@@ -0,0 +1,91 @@
+package kitchen.dev.books_proto;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.design.widget.FloatingActionButton;
+import android.widget.TextView;
+
+/**
+ * Created by noc on 14.02.16.
+ */
+public class IntroPageFragment extends Fragment{
+
+    public static final String ARG_PAGE = "page";
+
+    /**
+     * The fragment's page number, which is set to the argument value for {@link #ARG_PAGE}.
+     */
+    private int pageNumber;
+
+    /**
+     * Factory method for this fragment class. Constructs a new fragment for the given page number.
+     */
+    public static IntroPageFragment create(int pageNumber) {
+        IntroPageFragment fragment = new IntroPageFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, pageNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public IntroPageFragment() {
+    }
+
+    //on create to get arguments from factory method
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageNumber = getArguments().getInt(ARG_PAGE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout containing a title and body text.
+        ViewGroup rootView = (ViewGroup) inflater
+                .inflate(R.layout.fragment_intro_page, container, false);
+
+        // Set the title view to show the page number.
+        TextView introText = (TextView) rootView.findViewById(R.id.intro_text);
+
+        FloatingActionButton introButton = (FloatingActionButton) rootView.findViewById(R.id.intro_button);
+
+        introButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                SharedPreferences sharedPref = getActivity().getSharedPreferences("introDone",Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putBoolean("INTRO_DONE", true);
+                editor.commit();
+
+                Intent intent = new Intent(getActivity(), ItemListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        switch (pageNumber){
+            case 0: introText.setText(getString(R.string.intro_text_start));
+                introButton.setVisibility(View.INVISIBLE);
+                break;
+            case 1: introText.setText(getString(R.string.intro_text_howto));
+                introButton.setVisibility(View.INVISIBLE);
+                break;
+            case 2: introText.setText(getString(R.string.intro_text_go)); break;
+        }
+
+        return rootView;
+    }
+
+    /**
+     * Returns the page number represented by this fragment object.
+     */
+    public int getPageNumber() {
+        return pageNumber;
+    }
+}
