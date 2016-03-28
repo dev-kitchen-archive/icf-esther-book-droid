b25c2f8f33983fd9b0997a222e9197b90c1201b7
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2016-03-28 12:50:52 +0200

    close #13: getter and setter for model classes

--- app/src/main/java/kitchen/dev/icfbooks/model/books/Book.java
+++ app/src/main/java/kitchen/dev/icfbooks/model/books/Book.java
@@ -9,4 +9,36 @@ public class Book {
     private String author;
     private String description;
     private String image_url;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
 }
--- app/src/main/java/kitchen/dev/icfbooks/model/chapters/Chapter.java
+++ app/src/main/java/kitchen/dev/icfbooks/model/chapters/Chapter.java
@@ -8,4 +8,36 @@ public class Chapter {
     private String title;
     private int book_id;
     private int number;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setBook_id(int book_id) {
+        this.book_id = book_id;
+    }
+
+    public int getBook_id() {
+        return book_id;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
