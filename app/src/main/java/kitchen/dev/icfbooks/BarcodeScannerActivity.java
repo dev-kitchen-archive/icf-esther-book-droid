518e547697f536e410b9028531d8a5196acd8972
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2016-04-19 22:37:46 +0200

    fixed #15 by implementing permission request for API >= 23

--- app/src/main/java/kitchen/dev/icfbooks/BarcodeScannerActivity.java
+++ app/src/main/java/kitchen/dev/icfbooks/BarcodeScannerActivity.java
@@ -1,12 +1,17 @@
 package kitchen.dev.icfbooks;
 
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.zxing.ResultPoint;
 import com.journeyapps.barcodescanner.BarcodeCallback;
@@ -18,6 +23,7 @@ import java.util.List;
 public class BarcodeScannerActivity extends AppCompatActivity {
 
     private CompoundBarcodeView barcodeView;
+    public static final int CAMERA_PERMISSION_REQUEST_CODE = 3;
 
     private BarcodeCallback callback = new BarcodeCallback() {
         @Override
@@ -37,10 +43,27 @@ public class BarcodeScannerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        checkRequestPermissionForCamera();
+
         setContentView(R.layout.activity_barcode_scanner);
 
         barcodeView = (CompoundBarcodeView) findViewById(R.id.barcode_scanner);
         barcodeView.decodeContinuous(callback);
+    }
+
+
+    /**
+     * checks the permission and request it if necessary
+     */
+    private void checkRequestPermissionForCamera(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+            return;
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)){
+            Toast.makeText(this, "Camera permission needed. Please allow in App Settings for additional functionality.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},CAMERA_PERMISSION_REQUEST_CODE);
+        }
     }
 
     @Override
