c37c51879e318c19e89c4e9aa743b1cd11c366a6
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2016-04-19 13:48:32 +0200

    -modified some splashscreen code for running setup asynchronously - implemented traslation for German and English: closed #10

--- app/src/main/java/kitchen/dev/icfbooks/model/books/BookFactory.java
+++ app/src/main/java/kitchen/dev/icfbooks/model/books/BookFactory.java
@@ -15,7 +15,30 @@ public class BookFactory {
         //return Ester book hardcoded
         List<Book> books = new ArrayList<>();
         books.add(new Book(){
+            @Override
+            public String getTitle() {
+                return "Esther";
+            }
 
+            @Override
+            public String getAuthor() {
+                return "Leo Bigger";
+            }
+
+            @Override
+            public String getDescription() {
+                return "Esther in da House";
+            }
+
+            @Override
+            public String getImage_url() {
+                return "http://thesource.icf-download.ch/icf_zurich/01_GenX/2015/02_Esther/04_Multimedia/01_Design/JPEGS/esther-seriendesign.jpg";
+            }
+
+            @Override
+            public int getId() {
+                return 1;
+            }
         });
 
         return books;
