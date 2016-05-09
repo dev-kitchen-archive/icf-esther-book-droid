ad96e4e0db86b57677f4db7971b03282b3456582
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2016-05-09 07:48:35 +0200

    renamed ApiHelper to ApiClient #6

--- app/src/main/java/kitchen/dev/icfbooks/dal/ApiClient.java
+++ app/src/main/java/kitchen/dev/icfbooks/dal/ApiClient.java
@@ -1,47 +1,38 @@
 package kitchen.dev.icfbooks.dal;
 
 import android.content.Context;
-import android.os.AsyncTask;
-import android.os.Handler;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.DiskBasedCache;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.HashMap;
 
-import kitchen.dev.icfbooks.model.books.Book;
 import kitchen.dev.icfbooks.model.chapters.Chapter;
 import kitchen.dev.icfbooks.model.media.Media;
 
 /**
  * Created by noc on 19.02.16.
  */
-public class ApiHelper {
+public class ApiClient {
     private static final String BASE_URL = "https://rhino.dev.kitchen/de/";
 
     private RequestQueue queue;
-    private static ApiHelper instance;
-    private static HashMap<Context, ApiHelper> instances = new HashMap<>();
+    private static ApiClient instance;
+    private static HashMap<Context, ApiClient> instances = new HashMap<>();
 
-    private ApiHelper(Context context) {
+    private ApiClient(Context context) {
         this.queue = Volley.newRequestQueue(context);
     }
 
-    public static synchronized ApiHelper getInstance(Context context) {
+    public static synchronized ApiClient getInstance(Context context) {
         if (!instances.containsKey(context))
-            instances.put(context, new ApiHelper(context));
+            instances.put(context, new ApiClient(context));
 
         return instances.get(context);
     }
